34917ef08674274a1958abc7f5532b2dce40e2d8
José Víctor Ardasse <josevictorps3lima@gmail.com>
2023-12-08 13:20:30 -0300

    fix: try catch at POST and CORS definition

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
 
 // Adicione o middleware CORS para permitir solicitações de qualquer origem
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://api-csv-xp.onrender.com/",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: false,
+  })
+);
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(express.json()); // Parse do corpo da requisição como JSON
 
@@ -14,29 +20,31 @@ app.use(express.json()); // Parse do corpo da requisição como JSON
 let storedData = null;
 
 // Rota para receber dados em /assessores (POST)
-app.post('/assessores', (req, res) => {
-    const jsonData = req.body; // Dados JSON enviados no corpo da requisição
-
-    // Armazena os dados recebidos
+app.post("/assessores", (req, res) => {
+  try {
+    const jsonData = req.body;
     storedData = jsonData;
-
-    res.json({ message: 'Dados recebidos com sucesso!' });
+    res.json({ message: "Dados recebidos com sucesso!" });
+  } catch (error) {
+    console.error("Erro ao processar a requisição POST:", error);
+    res.status(500).json({ error: "Erro interno do servidor" });
+  }
 });
 
 // Rota para obter os dados armazenados em /assessores (GET)
-app.get('/assessores', (req, res) => {
-    // Retorna os dados armazenados (ou qualquer outro processamento necessário)
-    res.json(storedData);
+app.get("/assessores", (req, res) => {
+  // Retorna os dados armazenados (ou qualquer outro processamento necessário)
+  res.json(storedData);
 });
 
 // Rota de Delete
-app.delete('/delete-assessores', (req, res) => {
-    // Lógica para deletar os arquivos (exemplo simples)
-    storedData = null;
+app.delete("/delete-assessores", (req, res) => {
+  // Lógica para deletar os arquivos (exemplo simples)
+  storedData = null;
 
-    res.json({ message: 'Arquivos deletados com sucesso!' });
+  res.json({ message: "Arquivos deletados com sucesso!" });
 });
 
 app.listen(port, () => {
-    console.log(`Servidor Node.js rodando em http://localhost:${port}`);
+  console.log(`Servidor Node.js rodando na porta: ${port}`);
 });
